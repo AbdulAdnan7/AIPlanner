1fe362037875daac8b0dec6bbcd7b8392d5d009c
Abdul Adnan <peerabduladnan@gmail.com>
2025-08-03 21:58:53 +0530

    Adding Heroo-Section

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <main className="h-screen flex items-center justify-center bg-gradient-to-b from-white to-purple-200">
-      <h1 className="text-white text-4xl font-bold">
-        Hello
+    <main className="h-screen flex bg-gradient-to-b from-white to-purple-200">
+      <div className="px-8 mt-20 md:mt-30">
+      <h1 className='text-4xl font-semibold'>
+      Your Personal AI Trip Planner
       </h1>
+      <p className='text-1xl'>Plan your adventure effortesly with the power of AI</p>
+      </div>
     </main>
   )
 }
