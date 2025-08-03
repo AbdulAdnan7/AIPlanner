63fd980c27dbca391a659447ccfbfadd21277a92
Abdul Adnan <peerabduladnan@gmail.com>
2025-08-03 20:05:27 +0530

    added colors

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <main className="h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 via-pink-500 to-purple-700">
+    <main className="h-screen flex items-center justify-center bg-gradient-to-b from-white to-purple-200">
       <h1 className="text-white text-4xl font-bold">
         Hello
       </h1>
