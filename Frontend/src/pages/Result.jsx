444ad67c2056808bb9d407b01544b18b8528dc20
Abdul Adnan <peerabduladnan@gmail.com>
2025-08-04 19:18:36 +0530

    Improved Result

--- Frontend/src/pages/Result.jsx
+++ Frontend/src/pages/Result.jsx
@@ -17,7 +17,7 @@ const Result = () => {
   if (!itinerary) return null;
 
   return (
-    <div className="max-w-3xl mx-auto p-6  mt-20">
+    <div className="max-w-3xl mx-auto p-6  mt-20 mb-10">
       <h2 className="text-2xl font-bold mb-4 text-purple-700">Your Trip Itinerary</h2>
       <div className="prose prose-lg max-w-none">
         <ReactMarkdown>{itinerary}</ReactMarkdown>
