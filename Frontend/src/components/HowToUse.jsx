92f196f446813b9bf3cccbf1e778b484b259a67d
Abdul Adnan <peerabduladnan@gmail.com>
2025-08-11 12:45:52 +0530

    Added Guidance

--- Frontend/src/components/HowToUse.jsx
+++ Frontend/src/components/HowToUse.jsx
@@ -1,12 +1,45 @@
+import { Edit, MousePointerClick, Notebook, Sparkles } from 'lucide-react'
 import React from 'react'
 
 const HowToUse = () => {
+  const Guidance = [
+    {
+      title: 'Click the "Plan Trip" button',
+      icon: <MousePointerClick />
+    },
+    {
+      title: 'Enter your trip details',
+      icon: <Edit />
+    },
+    {
+      title: 'AI (Gemini) generates itinerary',
+      icon: <Sparkles />
+    },
+    {
+      title: 'View your trip results',
+      icon: <Notebook />
+    }
+  ]
+
   return (
-   <>
-    <div className='flex justify-center'>
-      <h1 className='text-center text-2xl font-semibold'>How To Use</h1>
-    </div>
-   </>
+    <>
+      <div className='flex justify-center mt-6'>
+        <h1 className='text-center text-2xl font-semibold'>How To Use</h1>
+      </div>
+      <div className='flex justify-center m-4 mt-2'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 w-full'>
+          {Guidance.map((guide, index) => (
+            <div
+              key={index}
+              className='bg-slate-50 p-6 flex flex-col items-start justify-center space-y-3 shadow-lg hover:shadow-xl transition rounded-xl'
+            >
+              <div className='text-purple-600'>{guide.icon}</div>
+              <h3 className='text-lg font-semibold'>{guide.title}</h3>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   )
 }
 
--- Frontend/src/components/Why.jsx
+++ Frontend/src/components/Why.jsx
@@ -2,17 +2,16 @@ import React from 'react'
 import { Compass, DollarSign, MapPin, Users } from 'lucide-react'
 
 const WhySection = () => {
-
-   const features = [
+  const features = [
     {
       title: 'Smart Itineraries',
       desc: 'AI creates tailored travel plans for your styles, budget, and schedule.',
-      icon: <MapPin  className='text-[#8B5CF6]' />
+      icon: <MapPin className='text-[#8B5CF6]' />
     },
     {
       title: 'Budget Optimizer',
       desc: 'Maximize experiences while keeping costs in check with AI powered savings',
-      icon: <DollarSign className='text-[#8B5CF6]'  />
+      icon: <DollarSign className='text-[#8B5CF6]' />
     },
     {
       title: 'Local Insights',
@@ -21,30 +20,30 @@ const WhySection = () => {
     },
     {
       title: 'Epic Group Trips',
-      desc: 'Coordinates with friends, split expenses, and discover exciting groupt activities',
+      desc: 'Coordinates with friends, split expenses, and discover exciting group activities',
       icon: <Users className='text-[#8B5CF6]' />
     }
-   ]
+  ]
 
   return (
     <>
-    <div className='flex justify-center'>
-      <h1 className='text-center text-2xl font-semibold'>Why Choose AI</h1>
-    </div>
-    <div className='h-screen flex justify-center m-4 items-center'>
-    <div className='grid h-full w-full grid-cols-4 grid-rows-4 gap-4'>
-     {features.map((feature, index) => (
-  <div
-    key={index}
-    className="col-span-2 lg:col-span-1 row-span-1 bg-slate-50 rounded-3xl p-6 flex flex-col items-start justify-center space-y-3 shadow-lg hover:shadow-xl transition"
-  >
-    <div className="text-indigo-600">{feature.icon}</div>
-    <h3 className="text-lg font-semibold">{feature.title}</h3>
-    <p className="text-sm text-gray-700">{feature.desc}</p>
-  </div>
-))}
-    </div>
-    </div>
+      <div className='flex justify-center mt-4'>
+        <h1 className='text-center text-2xl font-semibold'>Why we should Use</h1>
+      </div>
+      <div className='flex justify-center m-4 mt-2'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 w-full'>
+          {features.map((feature, index) => (
+            <div
+              key={index}
+              className='bg-slate-50 p-6 flex flex-col items-start justify-center space-y-3 shadow-lg hover:shadow-xl transition rounded-xl'
+            >
+              <div className='text-purple-600'>{feature.icon}</div>
+              <h3 className='text-lg font-semibold'>{feature.title}</h3>
+              <p className='text-sm text-gray-700'>{feature.desc}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   )
 }
