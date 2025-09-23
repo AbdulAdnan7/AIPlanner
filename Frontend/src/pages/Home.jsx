a0fa8024fc11b042dfc86c73a3b2f0c0ebfa3a4f
Abdul Adnan <peerabduladnan@gmail.com>
2025-09-23 16:19:13 +0530

    Removed Unncessary Sections

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
-import WhySection from '../components/Why'
-import HowToUse from '../components/HowToUse'
-import Footer from '../components/Footer'
+
 
 const Home = () => {
   return (
   <>
   <HeroSection /> 
-  <HowToUse />
-  <WhySection />
-  <Footer />
   </>
   )
 }
