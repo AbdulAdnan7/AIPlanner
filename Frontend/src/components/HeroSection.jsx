9b83f45b2663acc793a63e6c3b16e6a16aa03ab0
Abdul Adnan <peerabduladnan@gmail.com>
2025-09-23 15:40:16 +0530

    Improved Hero section

--- Frontend/src/components/HeroSection.jsx
+++ Frontend/src/components/HeroSection.jsx
@@ -14,12 +14,37 @@ const Home = () => {
 
   return (
     <>
-     <section className='max-w-full h-full'>
-     <div className='min-h-full text-7xl'>
-        Plan Ahead With Ease.
-           Using Our AI Trip Planner.
-     </div>
-     </section>
+   <section className='h-screen flex mt-16 justify-center p-6 ml-4 md:p-4' >
+  <div>
+    <h1 className='text-3xl text-center'>Plan Ahead With Ease. Using Our <span className='text-[#4169E1] font-semibold'> AI Trip Planner</span></h1>
+    <p className='text-center mt-4 font-semibold ml-4'>Let our AI create a personalized itinerary for your next adventure.</p>
+    <div className='flex justify-center items-center mt-4'>
+
+  <NavLink to={'/planTrip'}>
+ <button className="bg-[#4169E1] p-4 rounded text-white hover:bg-[#3556c7] transition-colors duration-300">
+  Generate Now
+</button>
+</NavLink>
+
+    </div>
+
+  </div>
+  <div>
+
+  </div>
+   </section>
+
+{
+  /**
+   * <section class="h-screen flex items-center justify-center bg-gray-100">
+  <div class="text-center">
+    <h1 class="text-5xl font-bold mb-4">Welcome to My Website</h1>
+    <p class="text-lg text-gray-700">This is the hero section subtitle</p>
+  </div>
+</section>
+
+   */
+}
     </>
   );
 };
