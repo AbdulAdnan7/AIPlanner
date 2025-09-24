e5fa0f50b3882593d2678f370b258f281ca49179
Abdul Adnan <peerabduladnan@gmail.com>
2025-09-24 14:02:21 +0530

    Improved layout

--- Frontend/src/components/StatCounter.jsx
+++ Frontend/src/components/StatCounter.jsx
@@ -6,9 +6,9 @@ export default function StatsCounter() {
     <section className="py-12">
       <div className="max-w-5xl mx-auto text-center flex gap-4 justify-evenly">
         <div className="flex items-center justify-center gap-4">
-          <FaMapMarkedAlt className="text-5xl text-blue-600" />
+          <FaMapMarkedAlt className="text-3xl md:text-5xl text-blue-600" />
           <div>
-            <h2 className="text-4xl font-bold text-blue-600">
+            <h2 className="text-2xl md:text-4xl font-bold text-blue-600">
               <CountUp end={1000} duration={2} />+
             </h2>
             <p className="text-lg font-medium text-blue-600">
@@ -17,9 +17,9 @@ export default function StatsCounter() {
           </div>
         </div>
            <div className="flex items-center justify-center gap-4">
-          <FaHotel className="text-5xl text-blue-600" />
+          <FaHotel className="text-3xl md:text-5xl text-blue-600" />
           <div>
-            <h2 className="text-4xl font-bold text-blue-600">
+            <h2 className="text-2xl md:text-4xl  font-bold text-blue-600">
               <CountUp end={50} duration={2} />+
             </h2>
             <p className="text-lg font-medium text-blue-600">
